17360ab67183ef186c8bc9737776f7940ea3f328
Dhruvin Prajapati <dhruvinhprajapati@gmail.com>
2023-04-25 11:07:46 +0530

    fix: added icons

---
+++ icons/bgIcons/index.js
@@ -0,0 +1,11 @@
+import { ReactComponent as HalfCircleIcon } from "./b1.svg";
+import { ReactComponent as TriangleIcon } from "./b2.svg";
+import { ReactComponent as LineIcon } from "./b3.svg";
+import { ReactComponent as DoubleHalfCircleIcon } from "./b4.svg";
+
+export default {
+  HalfCircleIcon,
+  TriangleIcon,
+  LineIcon,
+  DoubleHalfCircleIcon,
+};
